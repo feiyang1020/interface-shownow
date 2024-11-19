6754e9c356f00cffc05e63141a9579753736d059
feiyang1020 <feiyangl1020@163.com>
2024-11-19 16:00:57 +0800

    fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -29,8 +29,8 @@ export const DASHBOARD_API = "https://testnet.show.now/api";
 // export const DASHBOARD_API = "http://127.0.0.1:3000/api";
 // export const DASHBOARD_API = "/api";
 export const DASHBOARD_TOKEN = "DASHBOARD_TOKEN";
-// export const BASE_MAN_URL = "https://man-test.metaid.io";
-export const BASE_MAN_URL = "http://127.0.0.1:3000/man";
+export const BASE_MAN_URL = "https://man-test.metaid.io";
+// export const BASE_MAN_URL = "http://127.0.0.1:3000/man";
 // export const BASE_MAN_URL = "/man";
 export const FLAG = "metaid";
 
